fea47181a1ec875fa459db1f636269e948e6c0a3
Mihai Lazar <sharpoverride@gmail.com>
2017-03-19 10:24:02 +0200

    Add cancelled ajax requests sample

--- 7_operators/operators.js
+++ 7_operators/operators.js
@@ -12,6 +12,7 @@ const pipeArray = [
 ];
 
 // Pipe effect
+/*
 Rx.Observable
   .zip(Rx.Observable.interval(500), 
        Rx.Observable.from(pipeArray))
@@ -22,3 +23,22 @@ Rx.Observable
   .subscribe((word) => {
     console.log(word);
 });
+*/
+
+// Cancelled ajax requests sample
+Rx.Observable.zip(
+  Rx.Observable.interval(1000),
+  Rx.Observable.from(
+    [
+      'http://localhost:3000/users/1',
+      'http://localhost:3000/users/2',
+      'http://localhost:3000/users/3'
+    ])
+)
+  .do(valueAndUri => console.log(valueAndUri))
+  .switchMap(valueAndUri => Rx.Observable.ajax(valueAndUri[1]))
+  .subscribe(
+  (res) => console.log(res.response),
+  (err) => console.log(`Something went wrong ${err}`),
+  () => console.log('All ajax requests have finished')
+  );
